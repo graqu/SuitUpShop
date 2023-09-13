eadb413740efe251b90413f46a46a30fc4d64c63
Jacek <jacek_135@yahoo.pl>
2023-09-13 09:05:54 +0200

    footer link modification

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,23 @@
-import styles from "./Footer.module.css";
-import FooterInfo from "./FooterInfo";
+import styles from './Footer.module.css';
+import FooterInfo from './FooterInfo';
 
 const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-      <FooterInfo />
-      <div>Copyright © 2023 | Jacek Wiącek</div>
-    </footer>
-  );
+	return (
+		<footer className={styles.footer}>
+			<FooterInfo />
+			<div>
+				Copyright © 2023 |{' '}
+				<a
+					href='https://itjacek.waw.pl'
+					rel='noopener noreferrer'
+					target='_blank'
+					className={styles.link}
+				>
+					Jacek Wiącek
+				</a>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
